933fa010bf67a773e49cd3331b9a137281f8b147
Aleksei Belinskiy <escalion86@gmail.com>
2021-05-20 18:38:29 +0700

    Finished visual part

---
+++ src/components/BrandBar.js
@@ -0,0 +1,26 @@
+import { observer } from 'mobx-react-lite'
+import React, { useContext } from 'react'
+import { Row, Card } from 'react-bootstrap'
+import { Context } from '../index'
+
+const BrandBar = observer(() => {
+  const { device } = useContext(Context)
+
+  return (
+    <Row className="d-flex">
+      {device.brands.map((brand) => (
+        <Card
+          key={brand.id}
+          style={{ cursor: 'pointer' }}
+          border={brand.id === device.selectedBrand.id ? 'danger' : 'light'}
+          onClick={() => device.setSelectedBrand(brand)}
+          className="p-3"
+        >
+          {brand.name}
+        </Card>
+      ))}
+    </Row>
+  )
+})
+
+export default BrandBar
---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react'
+import { Navbar, Nav, Button, Container } from 'react-bootstrap'
+import { Context } from '../index'
+import { NavLink, useHistory } from 'react-router-dom'
+import { SHOP_ROUTE } from '../utils/consts'
+import { observer } from 'mobx-react-lite'
+import { LOGIN_ROUTE, ADMIN_ROUTE } from '../utils/consts'
+
+const NavBar = observer(() => {
+  const { user } = useContext(Context)
+  const history = useHistory()
+
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>
+          КупиДевайс
+        </NavLink>
+        <Nav className="ml-auto" style={{ color: 'white' }}>
+          {user.isAuth ? (
+            <>
+              <Button
+                variant={'outline-light'}
+                onClick={() => history.push(ADMIN_ROUTE)}
+              >
+                Админ панель
+              </Button>
+              <Button
+                variant={'outline-light'}
+                className="ml-2"
+                onClick={() => history.push(LOGIN_ROUTE)}
+                // onClick={() => user.setIsAuth(false)}
+              >
+                Выйти
+              </Button>
+            </>
+          ) : (
+            <Button
+              variant={'outline-light'}
+              onClick={() => history.push(LOGIN_ROUTE)}
+            >
+              Войти
+            </Button>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+})
+
+export default NavBar
